8e11771860cb4561c77f4c2fe91cda2f52cb2df2
zakaryan2107 <zakaryan2107@...>
2024-05-23 00:36:39 +0400

    ArrayList Implementation

---
+++ List/ArrayList.java
@@ -0,0 +1,61 @@
+package List;
+
+public class ArrayList<E> implements List<E> {
+
+    public static final int CAPACITY = 16;
+
+    private E[] data;
+
+    private int size = 0;
+
+    public ArrayList () { this(CAPACITY); }
+
+    public ArrayList (int capacity) {
+        data = (E[]) new Object[capacity];
+    }
+
+    public int size () { return size; }
+
+    public boolean isEmpty () { return size == 0; }
+
+    public E get (int i) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        return data[i];
+    }
+
+    public E set (int i, E e) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        E temp = data[i];
+        data[i] = e;
+        return temp;
+    }
+
+    public void add (int i, E e) throws IndexOutOfBoundsException, IllegalStateException{
+        checkIndex(i, size+1);
+        if (size == data.length) 
+            throw new IllegalStateException("Array Is Full.");
+        for (int k = size - 1; k >= i; k--)
+            data[k+1] = data[k];
+        data[i] = e;
+        size++;
+    }
+
+    public void add (E e) throws IllegalStateException {
+        add (size, e);
+    }
+
+    public E remove (int i) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        E temp = data[i];
+        for (int k = i; k < size - 1; k++)
+            data[k] = data[k+1];
+        data[size-1] = null;
+        size--;
+        return temp;
+    }
+
+    protected void checkIndex(int i, int n) throws IndexOutOfBoundsException {
+        if (i < 0 || i >= n)
+            throw new IndexOutOfBoundsException("Illegal Index " + i);
+    }
+}
